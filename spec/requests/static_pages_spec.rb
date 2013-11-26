5d10f4cadedb8e883549f99b18e4928521603b73
Satoru <skofidis@gmail.com>
2013-11-26 21:07:27 +0000

    Prettier rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,55 +1,34 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
-let(:const_title) {"Ellinika Nea | "}
-
+  subject { page }
+  
   describe "Home page" do
-
-    it "has the page the title 'Greek News App'" do
-      visit root_path
-      expect(page).to have_content('Greek News App')
+    before {visit root_path}
+    
+    it { should have_content('Greek News App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title (' | Home') }
     end
-    it "should have the title 'Home'" do
-      visit root_path
-      expect(page).to have_title(const_title + "Home")
-    end
-  end
 
   describe "Help page" do
+    before {visit help_path}
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title(const_title + "Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About Page" do
-
-    it "is should habe the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
-  
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title(const_title + "About Us")
-    end
+    before {visit about_path}
+    
+    it { should have_content ('About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact Page" do
-    it "should have the content 'Epikoinwnia'" do
-      visit contact_path
-      expect(page).to have_content('Epikoinwnia')
-    end
-    
-    it "should have the title 'Epikoinwnia'" do
-      visit contact_path 
-      expect(page).to have_title(const_title + "Epikoinwnia")
-    end
+    before {visit contact_path}
+
+    it { should have_content('Epikoinwnia') }
+    it { should have_title(full_title('Epikoinwnia')) }
   end
 end
